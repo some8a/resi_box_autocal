a6b93d80887e36f2f3854f32c94e97a5f21ef11a
some8a <some8a@gmail.com>
2021-08-17 15:30:48 +0900

    error fixed

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelTeikou = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.numericUpDownStdCal = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownStdMeas = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownDmmCal = new System.Windows.Forms.NumericUpDown();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.labelGpib = new System.Windows.Forms.Label();
+            this.numericUpDownGpib = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStdCal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStdMeas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDmmCal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGpib)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTeikou
+            // 
+            this.labelTeikou.AutoSize = true;
+            this.labelTeikou.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelTeikou.Location = new System.Drawing.Point(204, 23);
+            this.labelTeikou.Name = "labelTeikou";
+            this.labelTeikou.Size = new System.Drawing.Size(378, 16);
+            this.labelTeikou.TabIndex = 6;
+            this.labelTeikou.Text = "標準器校正値　　　　DMM測定値　　　　　　DMM校正比";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM6";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 182);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(147, 325);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonStart.Location = new System.Drawing.Point(21, 23);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(148, 61);
+            this.buttonStart.TabIndex = 9;
+            this.buttonStart.Text = "CAL START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonStop.Location = new System.Drawing.Point(20, 90);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(148, 61);
+            this.buttonStop.TabIndex = 10;
+            this.buttonStop.Text = "CAL STOP";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // numericUpDownStdCal
+            // 
+            this.numericUpDownStdCal.DecimalPlaces = 2;
+            this.numericUpDownStdCal.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDownStdCal.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDownStdCal.Location = new System.Drawing.Point(207, 42);
+            this.numericUpDownStdCal.Maximum = new decimal(new int[] {
+            110000,
+            0,
+            0,
+            0});
+            this.numericUpDownStdCal.Name = "numericUpDownStdCal";
+            this.numericUpDownStdCal.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDownStdCal.TabIndex = 15;
+            this.numericUpDownStdCal.Value = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownStdMeas
+            // 
+            this.numericUpDownStdMeas.DecimalPlaces = 2;
+            this.numericUpDownStdMeas.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDownStdMeas.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDownStdMeas.Location = new System.Drawing.Point(344, 42);
+            this.numericUpDownStdMeas.Maximum = new decimal(new int[] {
+            110000000,
+            0,
+            0,
+            0});
+            this.numericUpDownStdMeas.Name = "numericUpDownStdMeas";
+            this.numericUpDownStdMeas.ReadOnly = true;
+            this.numericUpDownStdMeas.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDownStdMeas.TabIndex = 16;
+            // 
+            // numericUpDownDmmCal
+            // 
+            this.numericUpDownDmmCal.DecimalPlaces = 6;
+            this.numericUpDownDmmCal.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDownDmmCal.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDownDmmCal.Location = new System.Drawing.Point(491, 42);
+            this.numericUpDownDmmCal.Maximum = new decimal(new int[] {
+            110000000,
+            0,
+            0,
+            0});
+            this.numericUpDownDmmCal.Name = "numericUpDownDmmCal";
+            this.numericUpDownDmmCal.ReadOnly = true;
+            this.numericUpDownDmmCal.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDownDmmCal.TabIndex = 17;
+            this.numericUpDownDmmCal.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.richTextBoxOutput.Location = new System.Drawing.Point(207, 71);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.ReadOnly = true;
+            this.richTextBoxOutput.Size = new System.Drawing.Size(404, 436);
+            this.richTextBoxOutput.TabIndex = 19;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // labelGpib
+            // 
+            this.labelGpib.AutoSize = true;
+            this.labelGpib.Location = new System.Drawing.Point(22, 159);
+            this.labelGpib.Name = "labelGpib";
+            this.labelGpib.Size = new System.Drawing.Size(67, 12);
+            this.labelGpib.TabIndex = 21;
+            this.labelGpib.Text = "GPIB ADDR";
+            this.labelGpib.Click += new System.EventHandler(this.labelGpib_Click);
+            // 
+            // numericUpDownGpib
+            // 
+            this.numericUpDownGpib.Location = new System.Drawing.Point(95, 157);
+            this.numericUpDownGpib.Name = "numericUpDownGpib";
+            this.numericUpDownGpib.Size = new System.Drawing.Size(72, 19);
+            this.numericUpDownGpib.TabIndex = 22;
+            this.numericUpDownGpib.Value = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 519);
+            this.Controls.Add(this.numericUpDownGpib);
+            this.Controls.Add(this.labelGpib);
+            this.Controls.Add(this.richTextBoxOutput);
+            this.Controls.Add(this.numericUpDownDmmCal);
+            this.Controls.Add(this.numericUpDownStdMeas);
+            this.Controls.Add(this.numericUpDownStdCal);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.labelTeikou);
+            this.Name = "Form1";
+            this.Text = "SELF CALBRATION";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStdCal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStdMeas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDmmCal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGpib)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTeikou;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.NumericUpDown numericUpDownStdCal;
+        private System.Windows.Forms.NumericUpDown numericUpDownStdMeas;
+        private System.Windows.Forms.NumericUpDown numericUpDownDmmCal;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+        private System.Windows.Forms.Label labelGpib;
+        private System.Windows.Forms.NumericUpDown numericUpDownGpib;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.IO.Ports;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+using System.Threading;
+using NationalInstruments.VisaNS;
+
+                        
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string[] rsetting = {
+            "000000000000000000001", "000000000000000000010",
+            "000000000000000000011", "000000000000000000100",
+            "000000000000000000111", "000000000000000001000",
+            "000000000000000001111", "000000000000000010000",
+            "000000000000000011111", "000000000000000100000",
+            "000000000000000111111", "000000000000001000000",
+            "000000000000001111111", "000000000000010000000",
+            "000000000000011111111", "000000000000100000000",
+            "000000000000111111111", "000000000001000000000",
+            "000000000001111111111", "000000000010000000000",
+            "000000000011111111111", "000000000100000000000",
+            "000000000111111111111", "000000001000000000000",
+            "000000001111111111111", "000000010000000000000",
+            "000000011111111111111", "000000100000000000000",
+            "000000111111111111111", "000001000000000000000",
+            "000001111111111111111", "000010000000000000000",
+            "000011111111111111111", "000100000000000000000",
+            "000111111111111111111", "001000000000000000000",
+            "001111111111111111111", "010000000000000000000",
+            "011111111111111111111", "100000000000000000000"
+//            "111111111111111111111", "000000000000000000000"
+        } ;
+
+        private MessageBasedSession mbSession;
+        string tableLab = "抵抗No\t測定比（No0=1）\t校正値\n";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            richTextBoxOutput.Text = tableLab;
+
+        }
+
+        private void setResi(string seriNum)
+        {
+            try {
+                serialPort1.Open();
+                serialPort1.Write(seriNum);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            serialPort1.Close();
+        }
+
+        private double getTmpr()
+        {
+            double resp = 0;
+
+            try {
+                serialPort1.Open();
+                serialPort1.Write("T");
+                Thread.Sleep(100);
+                resp = double.Parse(serialPort1.ReadLine());
+                serialPort1.Close();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+
+            return resp;
+        }
+
+        private void serialReset()
+        {
+            string[] ptName = SerialPort.GetPortNames();
+
+            serialPort1.PortName =  ptName[0];
+
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+            try
+            {
+                serialPort1.Open();
+                Thread.Sleep(3000);
+                serialPort1.Close();
+                richTextBox1.Text = ptName[0] + "\n" + richTextBox1.Text + "\n";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void dmmReset()
+        {
+            try　//通信部分
+            {
+                mbSession = (MessageBasedSession)ResourceManager.GetLocalManager().Open("GPIB0::" + numericUpDownGpib.Value.ToString() + "::INSTR");
+                mbSession.Write("CONF:RES");
+                mbSession.Write("RES:NPLC 100");
+                mbSession.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("接続エラー\n" + ex.Message);
+            }
+        }
+
+        double dmmMeas()
+        {
+            double rtnData = 0;
+
+            try　//通信部分
+            {
+                mbSession = (MessageBasedSession)ResourceManager.GetLocalManager().Open("GPIB0::" + numericUpDownGpib.Value.ToString() + "::INSTR");
+                rtnData = double.Parse(mbSession.Query("READ?").Replace("\n", ""));//末尾にNOHMがつくのでNで分割
+                mbSession.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("接続エラー\n" + ex.Message);
+            }
+
+            richTextBox1.Text = rtnData.ToString() + "\n" + richTextBox1.Text;
+            return rtnData;
+        }
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            double[] dmmAve = new double[10];
+            double tempdata0 = 0, tempdata1 = 0;
+            double[] resiRatio = { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            double[] resiValue = new double[21];
+            double[] tmprValue = new double[21];
+            double data1024 = 0;
+            string saveStr = "";
+            int numberOfLoop;
+
+            buttonStop.Enabled = true;
+            buttonStart.Enabled = false;
+            richTextBoxOutput.Text = "";
+
+            dmmReset();
+
+            MessageBox.Show("DMMを校正します\nDMMに100kΩ標準抵抗器を接続してください\n\nOKを押すと校正開始します");
+
+            DmmCal();
+            if (buttonStop.Enabled == false) goto END_MEAS;
+
+            MessageBox.Show("ディジタル可変抵抗を自己校正します\nDMMにディジタル可変抵抗を接続してください");
+
+            serialReset();
+
+            for (int j = 0; j < rsetting.Length; j++)
+            {
+                setResi("R" + rsetting[j]);
+
+                if (j > 132) numberOfLoop = 5;
+                else numberOfLoop = 2;
+                for (int k = 0; k < numberOfLoop; k++)//同じ測定2周やって最後の１周だけ使う
+                {
+                    for (int i = 0; i < dmmAve.Length; i++)
+                    {
+                        if(sleepSec(1) == true) goto END_MEAS;
+                        if (richTextBox1.Visible == true) labelGpib.Text = j.ToString() + " " + k.ToString() + " " + i.ToString();
+                        else labelGpib.Text = "GPIB ADDR";
+                        dmmAve[i] = dmmMeas();
+                    }
+                }
+
+                if (j % 2 == 0)
+                {
+                    tempdata0 = dmmAve.Average();
+                }
+                else
+                {
+                    tmprValue[(j + 1) / 2] = getTmpr();
+                    tempdata1 = dmmAve.Average();
+                    if (j == 21) data1024 = dmmAve.Average();
+                    resiRatio[(j + 1) / 2] = tempdata1 / tempdata0 * resiRatio.Sum();
+                    richTextBoxOutput.Text += ((j + 1) / 2).ToString("       00") + "\t" + resiRatio[(j + 1) / 2].ToString("  000000.0000") + "  \t" + tmprValue[(j + 1) / 2].ToString("F2") + "\n";
+                }
+            }
+
+            richTextBoxOutput.Text = tableLab;
+            for (int i = 1; i < resiRatio.Length; i++)
+            {
+                resiValue[i] = (resiRatio[i] / resiRatio[11]) * (double)numericUpDownDmmCal.Value * data1024;
+                richTextBoxOutput.Text += i.ToString("       00") + "\t" + resiRatio[i].ToString("  000000.0000") + "  \t" + tmprValue[i].ToString("F2") + "\t" + resiValue[i].ToString("00000000.00") + "\n";
+                saveStr += resiValue[i].ToString() + "@" + tmprValue[i].ToString("F2") + ",\n";
+            }
+
+            DialogResult dr = MessageBox.Show("自己校正が完了しました\n\n結果を保存します", "", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                File.WriteAllText(@"C:\Users\denshi\Documents\caldata\" + DateTime.Now.ToString("yyyyMMddHHmm") + ".txt", saveStr);
+            }
+
+        END_MEAS:
+
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+        private void DmmCal()
+        {
+            double[] dmmStdData = new double[10];
+
+
+            for (int j = 0; j < 2; j++)
+            {
+                for (int i = 0; i < dmmStdData.Length; i++)
+                {
+                    if (richTextBox1.Visible == true) labelGpib.Text = j.ToString() + " " + i.ToString();
+                    else labelGpib.Text = "GPIB ADDR";
+                    sleepSec(1);
+                    dmmStdData[i] = dmmMeas();
+                }
+            }
+            numericUpDownStdMeas.Value = (decimal)dmmStdData.Average();
+
+            numericUpDownDmmCal.Value = numericUpDownStdCal.Value / numericUpDownStdMeas.Value;
+        }
+
+        bool sleepSec(double slp)
+        {
+            int slpms = (int)(slp * 10);
+            bool stopped = false;
+
+            for (int i = 0; i < slpms ; i++)
+            {
+                Thread.Sleep(100);
+                Application.DoEvents();
+                if (buttonStop.Enabled == false)
+                {
+                    stopped = true;
+                    break;
+                }
+            }
+            return stopped;
+        }
+
+        private void labelGpib_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Visible == false)
+                richTextBox1.Visible = true;
+            else
+                richTextBox1.Visible = false;
+        }
+    }
+
+}
